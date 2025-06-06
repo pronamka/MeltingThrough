64c1bb1192bf200d0f66ad3388e441db3b5a629a
pronamka <defender0508@gmail.com>
2025-06-06 18:10:07 +0700

    added basic movement logic: standing, running, jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,71 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerMovement: MonoBehaviour
 {
-    
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 2.5f;
+    private bool grounded = false;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private Rigidbody2D body;
+
+    private Animator animator;
+
+    private int scaleValue = 5;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 movement = moveAction.ReadValue<Vector2>();
+        HandleHorizontalMovement(movement.x);
+        HandleVerticalMovement(movement.y);
+
+        animator.SetBool("isRunning", movement.x != 0);
+        animator.SetBool("grounded", grounded);
+    }
+
+    private void HandleHorizontalMovement(float x)
+    {
+        if (x > 0.01f) transform.localScale = new Vector3(1, 1, 1) * scaleValue;
+        else if (x < -0.01f) transform.localScale = new Vector3(-1, 1, 1) * scaleValue;
+
+        body.linearVelocityX = x * speed;
+    }
+
+    private void HandleVerticalMovement(float y)
+    {
+        if (jumpAction.triggered && grounded)
+        {
+            body.linearVelocityY = jumpForce;
+            grounded = false;
+            animator.SetTrigger("isJumping");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        };
+    }
 }
